f7b01a05ae66bd9c489df19c0bdcd7cfbc921ba9
cymi-eng <eaiancymi@gmail.com>
2026-07-14 20:34:40 +0300

    feat: import sonner in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,14 @@ import "@fontsource/inter";
 import "@fontsource/atkinson-hyperlegible-next";
 
 import { AuthProvider } from "@/context/AuthContext";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <Toaster richColors position="top-right" />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
